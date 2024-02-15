96d4781cdd3cf634ebe33dd5217764d68c8164b5
Homeo <40612643+homeopatix@users.noreply.github.com>
2024-02-15 20:29:45 +0100

    Create GlobalsDE.lua

---
+++ GlobalsDE.lua
@@ -0,0 +1,58 @@
+------------------------------------------------------------------------------------------
+-- GlobalDE file
+-- Written by Homeopatix
+-- 17 avril 2021
+-- updated 28 decembre 2023
+------------------------------------------------------------------------------------------
+_G.T = {};
+	------------------------------------------------------------------------------------------
+	-- Francais --
+	------------------------------------------------------------------------------------------
+	T[ "PluginName" ] = "FoodAndDrinks";
+	T[ "PluginText" ] = "Von Homeopatix";
+	T[ "Loaded" ] = "Geladen";
+	T[ "Unactivated" ] = "Deaktiviert";
+	T[ "PluginOptionsWindowShow" ] = "Zeigt das Optionsfenster an";
+	------------------------------------------------------------------------------------------
+	-- help --
+	------------------------------------------------------------------------------------------
+	T[ "PluginHelp1" ] = "\n\nListe der Befehle:\n";
+	T[ "PluginHelp2" ] = "/Fo show - Schaufenster.\n";
+	T[ "PluginHelp3" ] = "/Fo hide - verstecke das Fenster.\n";
+	T[ "PluginHelp4" ] = "/Fo options - Zeigen Sie das Optionsfenster an.\n";
+	T[ "PluginHelp5" ] = "/Fo esc - Aktivieren oder deaktivieren Sie die Escape-Taste.\n";
+	T[ "PluginHelp6" ] = "/Fo clear - L\195\182schen Sie alle Verkn\195\188pfungen.\n";
+	T[ "PluginHelp7" ] = "/Fo alt - ALT-Taste gedr\195\188ckt halten, um das Symbol zu verschieben.\n";
+	T[ "PluginHelp8" ] = "/Fo lock um die Symbole zu sperren oder zu entsperren\n";
+	T[ "PluginHelp9" ] = "/Fo toggle - Fenster ein-oder ausblenden\n";
+	T[ "PluginHelp10" ] = "/Fo repos Afficher la position actuelle de l'icone\n";
+	T[ "PluginHelp11" ] = "/Fo repos xxx xxx positionner l'icone sur les coordonn\195\169es xxx et xxx\n\n";
+	T[ "PluginHelp12" ] = "Klicken Sie mit der rechten Maustaste auf das Symbol, um das Optionsfeld anzuzeigen";
+
+	------------------------------------------------------------------------------------------
+	-- command text  --
+	------------------------------------------------------------------------------------------
+	T[ "PluginWindowShow" ] = "Schaufenster.";
+	T[ "PluginWindowHide" ] = "verstecke das Fenster.";
+	T[ "PluginWindowClear" ] = "L\195\182schen Sie alle Verkn\195\188pfungen.";
+	T[ "PluginLocked" ] = "Gesperrte Symbole";
+	T[ "PluginUnlocked" ] = "Symbole freigeschaltet";
+	T[ "PluginWindowsAlwaysShowOn" ] = "Fenster immer eingeschaltet anzeigen";
+	T[ "PluginWindowsAlwaysShowOff" ] = "Fenster immer AUS anzeigen";
+	T[ "PluginEscEnable" ] = "Escape-Taste aktiviert";
+	T[ "PluginEscDesable" ] = "Escape-Taste deaktiviert";
+	T[ "PluginAltEnable" ] = "Alt-Taste aktiviert";
+	T[ "PluginAltDesable" ] = "Alt-Taste deaktiviert";
+	T[ "PluginPosition" ] = "Aktuelle Symbols Position";
+	T[ "PluginResize" ] = "Neupositionierung des Symbols auf ";
+	T[ "PluginUnload" ] = "Entladen Sie das Plugin mit : /plugins entladen ";
+	T[ "PluginReload" ] = "Laden Sie das Plugin mit : /plugins laden ";
+	------------------------------------------------------------------------------------------
+	-- Options --
+	------------------------------------------------------------------------------------------
+	T[ "PluginOptionsText" ] = "Optionsfenster";
+	T[ "PluginOption1" ] = "Anzahl der Zeilen";
+	T[ "PluginOption2" ] = "Anzahl der Steckpl\195\164tze pro Zeile";
+	T[ "PluginOption3" ] = " Zeigen Sie den Fensterrand an";
+	T[ "PluginOption4" ] = " Fenster immer eingeschaltet anzeigen";
+	T[ "PluginOptionValidate" ] = "\195\132nderungen validieren";
